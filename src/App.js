e08a5c4fb31131fbfc74d844e48a725b4514a5a8
sdmitrash <s.dmytrash@ftr.group>
2023-09-18 15:50:00 +0200

    init 4

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
         <meta charSet="utf-8" />
         <title>My Title</title>
         <link rel="canonical" href="http://mysite.com/example" />
-        {previewImage && <meta data-rh="true" property="og:image" content={previewImage} />}
-        {previewImage && <meta data-rh="true" name="twitter:image" content={previewImage} />}
+        {/*{previewImage && <meta data-rh="true" property="og:image" content={previewImage} />}*/}
+        {/*{previewImage && <meta data-rh="true" name="twitter:image" content={previewImage} />}*/}
       </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
